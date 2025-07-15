3adb85234f33b9b1ad9854be9eff58a016f8efa0
akh5l <xdteddypro@gmail.com>
2025-07-15 05:42:22 +0100

    Setup project structure and added simple static stack implementation

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "include/stack.h"
+
+int main() {
+
+  Stack stack;
+  stack_initialize(&stack);
+
+
+  char a[] = "hello!";
+
+  stack_push(&stack, a);
+
+
+  printf("Peeked: %s\n",(char*)stack_peek(&stack));
+
+  // printf("Popped: %d\n",*(int*)stack_pop(&stack));
+  // printf("Popped: %d\n",*(int*)stack_pop(&stack));
+  // printf("Popped: %d\n",*(int*)stack_pop(&stack));
+
+  // printf("Popped: %d\n",*(int*)stack_pop(&stack));
+
+
+  return 0;
+}
